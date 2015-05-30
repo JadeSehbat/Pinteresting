c3ed796eae09a3afc3d84393e620bb743171bd04
Jade Sehbat <jade@letsgetweddy.com>
2015-05-30 16:49:03 +0100

    Created Home & About Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root "pages#home"
+  get "about" =>"pages#about"
 
-  # You can have the root of your site routed with "root"
+  # The priority is based upon order of creation: first created -> highest priority.
+
   # root 'welcome#index'
 
   # Example of regular route:
